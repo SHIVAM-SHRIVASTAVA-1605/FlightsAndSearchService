cb05cbe7b7dde9fc5c49fb648194eccfaf378002
SHIVAM-SHRIVATSAVA-1605 <shivam091204@gmail.com>
2025-05-15 11:08:28 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,28 +1,57 @@
+const { where } = require('sequelize');
 const { City } = require('../models/index');
 
 class CityRepository {
+    
     async createCity({ name }) {
         try {
-            const city = await City.create({name});
+            const city = await City.create({
+                name
+            });
             return city;
-        } catch (error) {
+        } catch(error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
 
-    async deleteCity({ cityId }) {
+    async deleteCity(cityId) {
         try {
             await City.destroy({
                 where: {
                     id: cityId
                 }
             });
+            return true;
         } catch (error) {
+            console.log("Something wwent wrong in the repository layer");
             throw {error};
         }
     }
 
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch(error) {
+            console.log("Something weent wrong in the repository layer");
+            throw(error);
+        }
+    }
 
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPK(cityId);
+            return City;
+        } catch (error) {
+            console.log("Somthing went wrong in the repository layer");
+            throw(error);
+        }
+    }
 }
 
 module.exports = CityRepository;
