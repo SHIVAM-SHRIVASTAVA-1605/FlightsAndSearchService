a7a40d958c4ea2279a59c348199c86e928b2f2ca
SHIVAM-SHRIVATSAVA-1605 <shivam091204@gmail.com>
2025-05-20 17:05:33 +0530

    Added airport seeders

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const bodyParser = require("body-parser");
 const { PORT } = require('./config/serverConfig');
 const ApiRouters = require('./routes/index');
 
+const db = require('./models/index');
+
+const {City, Airport} = require('./models/index');
+
 const setupAndStartServer = async() => {
     
     //crete the express object
@@ -14,8 +18,31 @@ const setupAndStartServer = async() => {
 
     app.use('/api', ApiRouters);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`);
+        if(process.env.SYNC_DB) {
+            db.sequelize.sync({alter: true});
+        }
+
+        // const city =  await City.findOne( {
+        //     where: {
+        //         id: 1
+        //     }
+        // });
+        // const airports = await city.getAirports();
+        // // const newAirport = await Airport.create({
+        // //     name: 'Jindal Vijaynagar Airport',
+        // //     cityId: 4
+        // // });
+        // const newAirport = await Airport.findOne({
+        //     where: {
+        //         id: 4
+        //     }
+        // })
+        // await city.addAirport(newAirport);
+        // // await city.addAirport({
+        // //     name: 'Jindal Vijaynagar Airport'
+        // // })
     });
 }
 
