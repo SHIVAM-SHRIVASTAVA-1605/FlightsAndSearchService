65dce6fabceae37b2dd54ac920e459dab99de95b
SHIVAM-SHRIVATSAVA-1605 <shivam091204@gmail.com>
2025-05-23 11:56:27 +0530

    Added flight-create request middleware

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -4,6 +4,16 @@ const flightService = new FlightService();
 
 const create = async (req, res) => {
     try {
+        const flightRequestData = {
+            flightNumber: req.body.flightNumber,
+            airplaneId: req.body.airplaneId,
+            departureAirportId: req.body.departureAirportId,
+            arrivalAirportId: req.body.arrivalAirportId,
+            aarivalTime: req.body.aarivalTime,
+            departureTime: req.body.departureTime,
+            price: req.body.price
+        }
+
         const flight = await flightService.createFlight(req.body);
         return res.status(201).json({
             data: flight,
